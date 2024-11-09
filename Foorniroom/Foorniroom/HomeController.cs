bc53368614afcddd7d4357dd1ee81de06674abf1
Raisa <volovik.raisa.kam@gmail.com>
2024-11-09 11:12:56 +0200

    added new controller

---
+++ Foorniroom/Foorniroom/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foorniroom
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("hello")] // Маршрут для вызова метода по URL /hello
+        public IActionResult Hello()
+        {
+            return Content("Hello word!");
+        }
+    }
+}
