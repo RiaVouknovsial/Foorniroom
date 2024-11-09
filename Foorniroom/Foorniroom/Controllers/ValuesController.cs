c9b4927e22d1247a4e3cb4e3185653e00ed49dc9
Raisa <volovik.raisa.kam@gmail.com>
2024-11-09 12:41:10 +0200

    second repository

---
+++ Foorniroom/Foorniroom/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Foorniroom.Interfaces;
+using Foorniroom.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foorniroom.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IValueRepository _valueService;
+
+        public ValuesController(IValueRepository valueService)
+        {
+            _valueService = valueService;
+        }
+
+        [HttpGet("value")]
+        public IActionResult GetValue()
+        {
+            var value = _valueService.GetValue();
+            return Ok(value);
+        }
+    }
+}
---
+++ Foorniroom/Foorniroom/Services/ValueService.cs
@@ -0,0 +1,21 @@
+﻿using Foorniroom.Interfaces;
+
+namespace Foorniroom.Services
+{
+    public class ValueService:IValueRepository
+    {
+       
+            private readonly IValueRepository _valueRepository;
+
+            public ValueService(IValueRepository valueRepository)
+            {
+                 _valueRepository = valueRepository;
+            }
+
+            public string GetValue()
+            {
+                return _valueRepository.GetValue();
+            }
+      
+    }
+}
